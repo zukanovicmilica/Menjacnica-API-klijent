d6fa73a3afbc28c6a09eb46a22299afd11d88717
zukanovicmilica <zukanovic.milica@gmail.com>
2018-05-09 21:02:58 +0200

    Dodata funkcionalnost dugmetu Konvertuj

--- Menjacnica API klijent/src/Menjacnica.java
+++ Menjacnica API klijent/src/Menjacnica.java
@@ -16,16 +16,19 @@ import com.google.gson.JsonParser;
 import klaseJSON.Drzava;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JComboBox;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Menjacnica extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_1;
-	public LinkedList<Drzava> drzave= new LinkedList<>();
+	public LinkedList<Drzava> drzave = new LinkedList<>();
 
 	/**
 	 * Launch the application.
@@ -46,7 +49,7 @@ public class Menjacnica extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	
+
 	public Menjacnica() {
 		setResizable(false);
 		setTitle("Menjacnica");
@@ -56,7 +59,6 @@ public class Menjacnica extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
 
 		try {
 			String url = URLConnectionUtil.getContent("http://free.currencyconverterapi.com/api/v3/countries");
@@ -72,52 +74,81 @@ public class Menjacnica extends JFrame {
 			e.printStackTrace();
 		}
 
-		
 		JLabel lblIzValuteZemlje = new JLabel("Iz valute zemlje:");
 		lblIzValuteZemlje.setBounds(40, 40, 107, 24);
 		contentPane.add(lblIzValuteZemlje);
-		
+
 		JLabel lblUValutuZemlje = new JLabel("U valutu zemlje:");
 		lblUValutuZemlje.setBounds(263, 40, 101, 24);
 		contentPane.add(lblUValutuZemlje);
-		
+
 		JComboBox comboBox = new JComboBox();
 		comboBox.setBounds(40, 75, 131, 20);
 		contentPane.add(comboBox);
 		dodajListu(comboBox);
-		
+
 		JComboBox comboBox_1 = new JComboBox();
 		comboBox_1.setBounds(263, 75, 131, 20);
 		contentPane.add(comboBox_1);
 		dodajListu(comboBox_1);
-		
+
 		JLabel lblIznos = new JLabel("Iznos:");
 		lblIznos.setBounds(40, 143, 46, 14);
 		contentPane.add(lblIznos);
-		
+
 		JLabel lblIznos_1 = new JLabel("Iznos:");
 		lblIznos_1.setBounds(263, 143, 46, 14);
 		contentPane.add(lblIznos_1);
-		
+
 		textField = new JTextField();
 		textField.setBounds(40, 168, 131, 20);
 		contentPane.add(textField);
 		textField.setColumns(10);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setBounds(263, 168, 131, 20);
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
-		
+
 		JButton btnKonvertuj = new JButton("Konvertuj");
+		btnKonvertuj.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Drzava dr = (Drzava) comboBox.getSelectedItem();
+				String str = dr.getCurrencyId() + "_";
+				dr = (Drzava) comboBox_1.getSelectedItem();
+				str += dr.getCurrencyId();
+				String novi = str;
+				str = "http://free.currencyconverterapi.com/api/v3/convert?q=" + str;
+				try {
+					str = URLConnectionUtil.getContent(str);
+					JsonParser p = new JsonParser();
+					JsonObject object = p.parse(str).getAsJsonObject();
+					Gson g = new GsonBuilder().create();
+					int count = g.fromJson(object.getAsJsonObject("query").getAsJsonPrimitive("count"), int.class);
+					if (count == 0) {
+						JOptionPane.showMessageDialog(null, "Nemoguce je izvrsiti transakciju!", "Greska",
+								JOptionPane.ERROR_MESSAGE);
+						return;
+					}
+					double kurs = g.fromJson(
+							object.getAsJsonObject("results").getAsJsonObject(novi).getAsJsonPrimitive("val"),
+							double.class);
+					Double d = new Double(kurs * Double.parseDouble(textField.getText()));
+					textField_1.setText(d.toString());
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
 		btnKonvertuj.setBounds(163, 220, 107, 23);
 		contentPane.add(btnKonvertuj);
-		
+
 	}
-	
+
 	private void dodajListu(JComboBox ponudjeno) {
-		for(Drzava i:drzave)
+		for (Drzava i : drzave)
 			ponudjeno.addItem(i);
 	}
-	
+
 }
